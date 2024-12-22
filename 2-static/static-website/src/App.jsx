e4d6a3d7851604cd229fa10e02d374ca4d41ddac
Anson Lee <64633971+leehosanganson@users.noreply.github.com>
2024-12-22 18:37:31 +0000

    static web

---
+++ 2-static/static-website/src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from './components/NavBar.jsx'
+import MainContent from './components/Main.jsx'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <MainContent />
+    </>
+  )
+}
+
+export default App
---
+++ 2-static/static-website/src/components/Main.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+    return (
+        <main>
+            <h1>Func facts about React</h1>
+            <ul className="facts-list">
+                <li>Was first realease in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+
+export default MainContent
